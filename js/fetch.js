4f7cd8200322d6a16ce9828694d9119a7f2253d1
Million Yemane <wedi_ngus@yahoo.com>
2018-09-19 20:13:25 -0500

    adding files

--- js/fetch.js
+++ js/fetch.js
@@ -5,7 +5,7 @@ const loadOrnella=()=>{
       xhttp.onreadystatechange = function() {
           if (this.readyState == 4 && this.status == 200) {
             var response = JSON.parse(xhttp.responseText);
-            var people = response.ornella;
+            var people = response.ornellas;
             // console.log(people);
             // return people;
             let ornimage=[];
@@ -13,9 +13,7 @@ const loadOrnella=()=>{
                 let onimage=people[i];
                 // console.log("people", onimage);
                 ornimage+=`<div class="gallery">`;
-                 
                 ornimage+= `<img class="galies" src="${onimage.img}">`;
-               
                 ornimage+=`</div>`;
                 $('#four').html(ornimage);
             }
@@ -26,15 +24,36 @@ const loadOrnella=()=>{
       xhttp.send();
     };
  loadOrnella();
-//    const welcome=()=>{
-//     //    console.log("something");
-//    return loadOrnella().then(function(){
-//        console.log("something");
-//    });
-//    };
-//   welcome();
+// const loadQuote = () => {
+//     var qoates= new XMLHttpRequest();
+//     qoates.open("GET", "ornella.json");
+//     qoates.onload=function(){
+//         var ourQoate=JSON.parse(qoates.responseText);
+//        console.log(ourQoate);
+//        gallery(qoates);
+//     }
+//    qoates.send();
+// };
+// const gallery=(data)=> {
+//     console.log("hi");
+//     let choose=`<div>
+//      <div class="wedground"><a href="gallery.html"><img src="./img/wedding.png" alt="trianle aman" width="300px"></a></div>
+//      <div class="enground"><a href=""><img  class="eng p-2" src="./img/engagmet.png" alt="trianle aman" width="300px"></a></div>
+//      <div class="gradground"><a href=""><img src="./img/graduation.png" alt="trianle aman" width="300px"></a></div>
+//      <div class="lowerlogo"></div>
+
+//      <h1 id="article">hellow</h1>
+//      <div id="gallery-footer"></div>
+//      </div>`;
+//       $('#tri').append(choose);
+// };
+// gallery();
+
+
 
 module.exports = {
-    loadOrnella
+    loadOrnella,
+// loadQuote,
+
 };
 
